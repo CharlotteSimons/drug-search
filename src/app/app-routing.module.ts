6df17364c871687171cd9d88116301e566eaf4f6
CharlotteSimons <charlottesimons99@gmail.com>
2024-01-17 15:15:24 +0100

    initialization plus first components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ResultsPageComponent } from './results-page/results-page.component';
+import { DrugPageComponent } from './drug-page/drug-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'results', component: ResultsPageComponent },
+  { path: 'drugpage', component: DrugPageComponent },
+  // ... andere routes
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
